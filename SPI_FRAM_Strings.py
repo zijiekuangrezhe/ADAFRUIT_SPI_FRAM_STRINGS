413ab5154806fb451206b40ae6f91df848c6d709
bytezealot1337 <50769035+bytezealot1337@users.noreply.github.com>
2019-06-13 11:05:07 -0400

    Update SPI_FRAM_Strings.py

--- SPI_FRAM_Strings.py
+++ SPI_FRAM_Strings.py
@@ -1,4 +1,4 @@
-# SPI FRAM STUFF
+# SPI FRAM STRINGS STUFF
 import board
 import busio
 import digitalio
@@ -36,8 +36,38 @@ def spiFramReset(startAddress, resetLength):
                     fram[startAddress + position] = 0xff
                 elapsedTime = time.time() - startTime
                 print("-> {} Bytes Reset in about {} Seconds.".format(resetLength, elapsedTime))
+    return
 
-def spiFramWrite(stringToWrite, startAddress, startSeparator, endSeparator):
+def spiFramWrite(stringToWrite, startAddress, startSeparator, endSeparator): # Decide if string is short or long.
+    if(len(stringToWrite) < 968): # Safe Short String Value
+        spiFramWriteShortStrings(stringToWrite, startAddress, startSeparator, endSeparator)
+    else:
+        spiFramWriteLongStings(stringToWrite, startAddress, startSeparator, endSeparator)
+    return
+
+def spiFramWriteShortStrings(stringToWrite, startAddress, startSeparator, endSeparator):
+    if(startAddress + len(startSeparator) + len(stringToWrite) + len(endSeparator) > byteArrayRange - 1):
+        print("-> ERROR: Can't fit text into chosen space!"
+        + " startAddress must be less than or equal to {}. Missing {} Byte(s)."
+        .format(byteArrayRange - (len(startSeparator) + len(stringToWrite) + len(endSeparator)) - 1,
+        startAddress + len(startSeparator) + len(stringToWrite) + len(endSeparator) + 1 - byteArrayRange), end="")
+    else:
+        print("-> Writing {} Bytes from FRAM Address {} to {}..."
+        .format(len(startSeparator) + len(stringToWrite) + len(endSeparator), startAddress,
+        startAddress + len(startSeparator) + len(stringToWrite) + len(endSeparator)))
+        startTime = time.time()
+        for position in range(len(startSeparator)):
+            fram[startAddress + position] = bytearray(startSeparator[position])
+        fram[startAddress + len(startSeparator)] = bytearray(stringToWrite)
+        for position in range(len(endSeparator)):
+            fram[startAddress + len(startSeparator) + len(stringToWrite) + position] = bytearray(endSeparator[position])
+        fram[startAddress + len(startSeparator) + len(stringToWrite) + len(endSeparator)] = 0xff
+        elapsedTime = time.time() - startTime
+        print("-> {} Bytes Written in about {} Seconds.".format(len(stringToWrite), elapsedTime))
+        del(stringToWrite)
+    return
+
+def spiFramWriteLongStings(stringToWrite, startAddress, startSeparator, endSeparator):
     if(startAddress + len(startSeparator) + len(stringToWrite) + len(endSeparator) > byteArrayRange - 1):
         print("-> ERROR: Can't fit text into chosen space!"
         + " startAddress must be less than or equal to {}. Missing {} Byte(s)."
@@ -58,6 +88,7 @@ def spiFramWrite(stringToWrite, startAddress, startSeparator, endSeparator):
         elapsedTime = time.time() - startTime
         print("-> {} Bytes Written in about {} Seconds.".format(len(stringToWrite), elapsedTime))
         del(stringToWrite)
+    return
 
 def spiFramRead(startAddress):
     print("-> Reading & Printing FRAM Until First Empty Byte:", end="\n\n")
@@ -75,6 +106,7 @@ def spiFramRead(startAddress):
     elapsedTime = time.time() - startTime
     print("\n\n-> {} Bytes Read in about {} Seconds.".format(position, elapsedTime))
     del(stringToRead)
+    return
 
 def spiFramPrintWhole():
     print("-> Reading & Printing the Whole {} Bytes FRAM Data:".format(byteArrayRange), end="\n\n")
@@ -93,12 +125,14 @@ def spiFramPrintWhole():
         print("\n{END}", end="")
     elapsedTime = time.time() - startTime
     print("\n\n-> {} KB SPI FRAM Printed in about {} Seconds.".format(byteArrayRange, elapsedTime))
+    return
 
 
 stringToWrite = "Lorem ipsum dolor sit amet, quo oporteat nominati id, no meis disputationi eos, stet utroque vis at. Mel ferri adversarium no, illum consetetur pro ad, iusto commune mea ad. Errem voluptua vis at, eam malis aliquip imperdiet id. Simul partiendo sea ea, ne sed diam partem nostrud, eam dicat nonumes no. Duo impetus detraxit cu, doctus adipiscing ne vix.\n\nUsu cu modo malorum, eum no liber insolens temporibus. Ius no illud zril recteque, et ius mundi latine aperiri, at melius aperiam recusabo vix. Esse euismod eam te, zril civibus volumus ea nam, per eleifend indoctum an. At usu soluta comprehensam, no vix agam molestie, id sea wisi eripuit necessitatibus. Fugit appetere eum eu, ridens bonorum tractatos at has, est assentior pertinacia an. Eos eros omnesque et.\n\nUllum meliore at nec, ut erant fastidii his, cum cu causae aliquip recusabo. Option dolores ex eos, dicant neglegentur eam ei, ea quem vulputate contentiones vis. Ferri feugiat maiorum no vim. Id falli dicant pri. Has ei movet ceteros.\n\nIus case feugait te, integre sensibus in duo, per ad eirmod referrentur philosophia. Augue harum nullam te mei, diceret conceptam mea ut. Per cu illud perpetua torquatos. Ea usu atqui assueverit cotidieque. Te sit salutatus consulatu, vis graeci euripidis dissentiet id.\n\nSit quidam tamquam delicata eu, duo audiam perfecto eu. Sit ad harum fuisset gubergren. Ut dicit bonorum signiferumque est, pri scriptorem cotidieque persequeris ne. Eu quod dicant definitiones eam, ea vis graeco partiendo maiestatis."
+#stringToWrite = "A"*500
 
 print("-> SPI FRAM Size: {} Bytes = {} KB.".format(len(fram), len(fram) / 1024.0))
-spiFramReset(startAddress=0, resetLength=byteArrayRange)
+spiFramReset(startAddress=0, resetLength=8192)
 spiFramWrite(stringToWrite, startAddress=0, startSeparator="", endSeparator="")
 spiFramRead(0)
 #spiFramPrintWhole()
